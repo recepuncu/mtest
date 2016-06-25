6b8c65c9ef69e2f940578b0418d7239c69ad0677
RECEP UNCU <recepuncu@gmail.com>
2016-06-25 09:09:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,104 +1,99 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Place searches</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #map {
-        height: 100%;
-      }
-    </style>
-  </head>
-  <body>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1044549635583804',
-      xfbml      : true,
-      version    : 'v2.6'
-    });
-  };
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>GoogleMapsApi</title>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+<style type="text/css">
+html, body {
+	height: 100%;
+	margin: 0;
+	padding: 0;
+}
+#harita {
+	height: 100%;
+}
+</style>
+<script type="text/javascript" src="img/jquery-1.12.3.min.js"></script>
+</head>
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>	
+<body>
+
+<div id="harita"></div>
+
+<script type="text/javascript">
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '1044549635583804',
+		xfbml      : true,
+		version    : 'v2.6'
+	});
+};	
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 	
-    <script>
-      
-      var map;
-      var infowindow;
-	  var service;
+var harita;
+var ilk_konum;
+var infowindow;
+var placesService;	
 
-      function initMap() {
-        var pyrmont = {lat: 38.423734, lng: 27.142826};
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 13
-        });
-
-        infowindow = new google.maps.InfoWindow();
-        service = new google.maps.places.PlacesService(map);
+function initMap() {
+	$(function(){
+		ilk_konum = {lat: 38.423734, lng: 27.142826};
 		
-        /*service.nearbySearch({
-          location: pyrmont,
-          radius: 500,
-          type: ['store']
-        }, callback);*/
-        service.textSearch({
-          location: pyrmont,
-          radius: 500,
-          query: 'Gediz Elektrik'
-        }, callback);		
-      }
+		harita = new google.maps.Map($('#harita')[0], {
+			center: ilk_konum,
+			zoom: 13
+		});
+		
+		infowindow = new google.maps.InfoWindow();
+		placesService = new google.maps.places.PlacesService(harita);
+		
+		placesService.textSearch({
+			location: ilk_konum,
+			radius: 500,
+			query: 'Gediz Elektrik'
+		}, placesServiceCallback);		
+	});	
+}//initMap
 
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
-	  
-	  function getDetails(place){
-		service.getDetails({ reference: place.reference }, function(place, status) {
-			if (status === google.maps.places.PlacesServiceStatus.OK) {
-				console.log(place);
-			}
-		});	  
-	  }
+function placesServiceCallback(results, status) {
+	if (status === google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+			createPlaceMarker(results[i]);
+		}
+	}
+}//placesServiceCallback
 
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
+function getPlacesDetails(place){
+	placesService.getDetails({ reference: place.reference }, function(place, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			console.log(place);
+			infowindow.setContent(place.name);
+			infowindow.open(harita, this);				
+		}
+	});	  
+}//getPlacesDetails
 
-        google.maps.event.addListener(marker, 'click', function() {
-			//place.formatted_address
-			//place.icon
-			//place.photos[0].getUrl();
-			getDetails(place);
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
-    </script>
+function createPlaceMarker(place) {
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+		map: harita,
+		position: place.geometry.location
+	});
 	
-    <div id="map"></div>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLa4qA1ELbFMs2GN7FzAzdpT2QdPG38ds&libraries=places&callback=initMap" async defer></script>
-	
-  </body>
+	google.maps.event.addListener(marker, 'click', function() {
+		getPlacesDetails(place);
+	});
+}//createMarker
+
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLa4qA1ELbFMs2GN7FzAzdpT2QdPG38ds&libraries=places&callback=initMap" async defer></script>
+</body>
 </html>
