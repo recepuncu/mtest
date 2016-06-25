171ba63cddd2e9d9640cdea8f443a7db2184faf1
RECEP UNCU <recepuncu@gmail.com>
2016-06-25 09:38:13 +0300

    a

--- index.php
+++ index.php
@@ -74,8 +74,6 @@ function getPlacesDetails(place, marker){
 	placesService.getDetails({ reference: place.reference }, function(place, status) {
 		if (status === google.maps.places.PlacesServiceStatus.OK) {			
 			var _content = '';
-			if(typeof place.name !== 'undefined')
-				_content += place.name +'<br/>';
 			if(typeof place.formatted_address !== 'undefined')
 				_content += place.formatted_address +'<br/>';
 			if(typeof place.formatted_phone_number !== 'undefined')
@@ -90,7 +88,9 @@ function createPlaceMarker(place) {
 	var placeLoc = place.geometry.location;
 	var marker = new google.maps.Marker({
 		map: harita,
-		position: place.geometry.location
+		position: place.geometry.location,
+		title: place.name,
+		icon: 'https://mtes01.herokuapp.com/img/logo-gediz.png'
 	});
 	
 	google.maps.event.addListener(marker, 'click', function() {
